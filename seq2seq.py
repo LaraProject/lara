c5160acdbcf02ad035a924f15df48c96007f3549
Louis Popi <theh2o64@gmail.com>
2020-06-07 10:37:20 +0200

    lara: seq2seq: Import latest changes
    * Revision : 9790bff1b1d82430298552b94264290d808f4a59

--- seq2seq.py
+++ seq2seq.py
@@ -13,6 +13,7 @@ from gensim.models import Word2Vec
 import re
 from gensim.models import KeyedVectors
 import argparse
+from gensim.models import FastText
 
 # Global variables
 
@@ -35,12 +36,13 @@ def import_data():
 	z.extractall()
 
 # Import custom data
-def use_custom_data(path):
+def use_custom_data(path, size):
 	global questions
 	global answers
 	# Open file
 	f = open(path)
-	lines = f.readlines()
+	lines = (f.readlines())
+	lines = lines[:int(len(lines) * (size/100.))]
 	f.close()
 	non_tonkenized_answers = []
 	for i in range(len(lines)):
@@ -129,9 +131,12 @@ def clean_everything():
 	questions = [clean_text(s) for s in questions]
 	answers = [clean_text(s) for s in answers]
 
-def load_word2vec(model_path):
+def load_word2vec(model_path, useFastText):
 	global model_w2v
-	model_w2v = KeyedVectors.load_word2vec_format(model_path, binary=False)
+	if useFastText:
+		model_w2v = FastText.load(model_path)
+	else:
+		model_w2v = KeyedVectors.load_word2vec_format(model_path, binary=False)
 
 def fit_tokenizer():
 	global tokenizer
@@ -144,7 +149,7 @@ def fit_tokenizer():
 def get_known_words():
 	known_words = []
 	for word in tokenizer.word_index:
-		if word in model_w2v.vocab:
+		if word in model_w2v:
 			known_words.append(word)
 	return known_words + ["<unk>"]
 
@@ -336,6 +341,8 @@ argslist = argparse.ArgumentParser(description="Seq2Seq Neural Network")
 
 argslist.add_argument('word2vec_model', metavar='word2vec_model', type=str,
 		help='Path to the word2vec model')
+argslist.add_argument('--useFastText', metavar='[True/False]', type=bool,
+        help='Specify whether to use Facebook FastText model', default=False, required=True)
 argslist.add_argument('--downloadData', metavar='[True/False]', type=bool,
         help='Specify whether the dataset should be downloaded', default=False, required=False)
 argslist.add_argument('--customData', metavar='path', type=str,
@@ -354,6 +361,8 @@ argslist.add_argument('--useBatchNormalisation', metavar='[True/False]', type=bo
         help='Specify whether to use batch normalisation', default=True, required=False)
 argslist.add_argument('--vectorSize', metavar='size', type=int,
         help='Specify the size of the word vectors', default=100, required=False)
+argslist.add_argument('--dataSize', metavar='size', type=int,
+        help='Specify the percentage of data to use', default=100, required=False)
 args = argslist.parse_args()
 
 # Launch everything
@@ -368,7 +377,7 @@ if len(args.loadModel) > 0:
 else:
 	if len(args.customData) > 0:
 		print("Seq2Seq: Using custom dataset from " + args.customData)
-		use_custom_data(args.customData)
+		use_custom_data(args.customData, args.dataSize)
 	else:
 		if args.downloadData:
 			print("Seq2Seq: Downloading data")
@@ -378,7 +387,7 @@ else:
 		print("Seq2Seq: Cleaning the data...")
 		clean_everything()
 	print("Seq2Seq: Loading word2vec model...")
-	load_word2vec(args.word2vec_model)
+	load_word2vec(args.word2vec_model, args.useFastText)
 	print("Seq2Seq: Training the tokenizer")
 	fit_new_tokenizer()
 	print("Seq2Seq: Creating the embedding matrix...")
